4b5db1975681830489bf95a1f9d74798e48f78c8
dyshleva <116552489+dyshleva@users.noreply.github.com>
2023-10-12 14:35:32 +0300

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -12,11 +12,11 @@ public class FlowerPack {
         this.setQuantity(quantity);
     }
 
-    public void setQuantity(int quantity) {
-        if (quantity < 1) {
+    public void setQuantity(int value) {
+        if (value < 1) {
             this.quantity = 1;
         } else {
-            this.quantity = quantity;
+            this.quantity = value;
         }
     }
 
